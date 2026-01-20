570cd07f393eb4d50d7904e6de106341d9b68de3
tirth160 <tirthpatel1607@gmail.com>
2026-01-20 15:26:43 +0530

    Add files via upload

---
+++ asessement2.py
@@ -0,0 +1,67 @@
+# 1 Calculate the remainder of two numbers. 
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# print("Remainder:", a % b)
+
+# 2 Check if a number is even or odd.
+# n= int(input("Enter a number: "))
+# if nn % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# 3 Compare two numbers and print the larger one. 
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# if a > b:
+#     print("Larger number:", a)
+# elif b > a:
+#     print("Larger number:", b)
+# else:
+#     print("Both numbers are equal")
+
+# 4 Write a program to calculate the square and cube of a number.
+# n = int(input("Enter a number: "))
+# print("Square:", n ** 2)
+# print("Cube:", n ** 3)
+
+# 5 Check if two entered numbers are equal. 
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# print(a == b)
+
+# 6 Take two numbers and print True if both are positive, else False.
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# print(a > 0 and b > 0)
+
+# 7 Write a program to convert float to integer.
+# x = float(input("Enter a float number: "))
+# print("Integer value:", int(x))
+
+# 8 Take a number as string, convert to int, and multiply by 10.
+# s = input("Enter a number as string: ")
+# num = int(s)
+# print(num * 10)
+
+# 9 Write a program that uses and & or operators to check multiple conditions.
+# a = int(input("Enter a number: "))
+
+# if a > 0 and a % 2 == 0:
+#     print("Positive and Even")
+# elif a < 0 or a == 0:
+#     print("Negative or Zero")
+
+# 10 Divide two numbers and print the quotient and remainder separately.
+# a = int(input("Enter dividend: "))
+# b = int(input("Enter divisor: "))
+
+# quotient = a // b
+# remainder = a % b
+
+# print("Quotient:", quotient)
+# print("Remainder:", remainder)
+ 	
---
+++ assessment1.py
@@ -0,0 +1,53 @@
+# Write a program to print your name, age, and city in one line.
+# print("Name: Alex, Age: 20, City: New York")
+
+# 2. Take user input for two numbers and print their sum. 
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# print("Sum =", a + b)
+# 3. Write a program to convert temperature from Celsius to Fahrenheit.
+# c = float(input("Enter temperature in Celsius: "))
+# f = (c * 9/5) + 32
+# print("Temperature in Fahrenheit:", f)
+
+# 4. Store your name in a variable and print it in uppercase. 
+# name = "alex"
+# print(name.upper())
+
+# 5. Ask the user for their birth year and calculate their current age.
+# birth_year = int(input("Enter your birth year: "))
+# current_year = 2025
+# age = current_year - birth_year
+# print("Your age is:", age)
+
+
+#  6. Write a program to swap the values of two variables.
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# a, b = b, a
+
+# print("After swapping:")
+# print("a =", a)
+# print("b =", b)
+# 7. Create a program o calculate the area of a rectangle from user inputs.
+# length = float(input("Enter length: "))
+# width = float(input("Enter width: "))
+# area = length * width
+# print("Area of rectangle:", area)
+# 8. Write a program to check if a number is positive or negative.
+# num = int(input("Enter a number: "))
+
+# if num >= 0:
+#     print("Positive number")
+# else:
+#     print("Negative number")
+
+
+# 9. Ask for two numbers and print their average.
+# x = float(input("Enter first number: "))
+# y = float(input("Enter second number: "))
+# average = (x + y) / 2
+# print("Average =", average)
+
+
